eda4194d1225a44ab6cbc1a1cf62a8aeea7a082a
LucasTR85 <88382977+LucasTR85@users.noreply.github.com>
2022-11-02 07:41:34 -0300

    Add files via upload

---
+++ Atividade10/script.js
@@ -0,0 +1,16 @@
+var Aluno1 = new Object();
+Aluno1.ra = "0030482111021"
+Aluno1.nome = "Lucas Tadeu Ribeiro"
+
+var Aluno2 = {};
+Aluno2.ra = "0001112222333"
+Aluno2.nome = "José Santos"
+
+var Aluno3 = {
+    ra:"0005556666777",
+    nome:"Ana Luiza"
+};
+
+alert("Aluno 1:\n\n" + "RA: " + Aluno1.ra + "\nNome: " + Aluno1.nome);
+alert("Aluno 2:\n\n" + "RA: " + Aluno2.ra + "\nNome: " + Aluno2.nome);
+alert("Aluno 3:\n\n" + "RA: " + Aluno3.ra + "\nNome: " + Aluno3.nome);
